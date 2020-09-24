6f5febd9ee385adb96ecdfd3ecf9a39eb9503728
Justas <justas.puodzius@gmail.com>
2020-09-24 19:39:46 +0300

    Pasisveikinau Justas

--- GitBandymoPRograma/ConsoleApp1/Program.cs
+++ GitBandymoPRograma/ConsoleApp1/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sveiki visi!"); //Justas
+
+
+
+
+            Console.Read();
         }
     }
 }
