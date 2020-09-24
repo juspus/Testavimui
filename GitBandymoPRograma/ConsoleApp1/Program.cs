1d198659c89613f1350592b3010b2e2ab465fcc7
Agna <agna.semaskaite@hotmail.com>
2020-09-24 19:41:43 +0300

    AgnosPasisveikinimas

--- GitBandymoPRograma/ConsoleApp1/Program.cs
+++ GitBandymoPRograma/ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine("Labas vakars: Agna");
+            Console.ReadLine();
+
         }
     }
 }
